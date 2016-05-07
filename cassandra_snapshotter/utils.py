aa0336b2b1f2c335e3902742273580044489e511
mike-gauthiere <mike-gauthiere@users.noreply.github.com>
2016-05-07 14:35:35 -0600

    Add host to regions list for aws govcloud

--- cassandra_snapshotter/utils.py
+++ cassandra_snapshotter/utils.py
@@ -20,7 +20,8 @@ S3_CONNECTION_HOSTS = {
     'ap-southeast-1': 's3-ap-southeast-1.amazonaws.com',
     'ap-southeast-2': 's3-ap-southeast-2.amazonaws.com',
     'ap-northeast-1': 's3-ap-northeast-1.amazonaws.com',
-    'sa-east-1': 's3-sa-east-1.amazonaws.com'
+    'sa-east-1': 's3-sa-east-1.amazonaws.com',
+    'us-gov-west-1': 's3-fips-us-gov-west-1.amazonaws.com'
 }
 
 base_parser = argparse.ArgumentParser(
